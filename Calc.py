e102a090bb2f4be099db33720f5d977e036d00d2
Marija Vecovska <marija.vecovska@students.finki.ukim.mk>
2020-09-24 02:53:54 +0200

    Adding the initial files Calc and multiply

---
+++ Calc.py
@@ -0,0 +1,13 @@
+print("enter operand 1: ")
+o1 = int(input())
+print("enter operand 2: ")
+o2 = int(input())
+print("enter operator: ")
+oper = input()
+
+if oper == "+":
+    print(str(o1) + " + " + str(o2) + " = " + str(o1+o2))
+elif oper == "-":
+    print(str(o1) + " - " + str(o2) + " = " + str(o1-o2))
+else:
+    print("Invalid operator")
